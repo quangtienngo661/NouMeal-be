9a65469dcd41de16bd797c197d516663d5a47442
quangtienngo661 <quangtienngo661@gmail.com>
2026-01-22 01:07:24 +0700

    feat: revert CORS origin configuration to allow all origins for development

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -3,7 +3,8 @@ dotenv.config();
 
 // Simple CORS configuration for development
 exports.corsConfig = {
-  origin: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : '*',
+  origin: '*',
+  // origin: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: [
